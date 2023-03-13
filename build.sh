0c74c31f0f343b44a506f30a50a967a00c17344d
kehuo <hkhuoke@hotmail.com>
2023-03-13 20:51:13 +0800

    CMake Tutorial: Step5 Exercise 1

--- build.sh
+++ build.sh
@@ -5,4 +5,15 @@ cd ${BUILD_DIR}
 
 cmake .. -DUSE_MYMATH=ON
 
-cmake --build .
+# Basic use of install.
+cmake --install .
+
+# For multi-configuration tools, we can use the --config argument to specify the configuration.
+# cmake --install . --config Release
+
+# For IDE, simply build the INSTALL target.
+# cmake --build . --target install --config Debug
+
+# use --prefix to determine the root of where the files will be installed.
+# cmake --install . --prefix "/home/myuser/installdir"
+
